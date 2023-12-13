77f585c763af00dcbe4750ae9127eca6111184a6
Vitor Ribeiro <vitorhugomelo5@gmail.com>
2023-12-13 17:01:27 -0300

    criando tela de finanças

--- src/pages/dashboard/finance/page.tsx
+++ src/pages/dashboard/finance/page.tsx
@@ -1,15 +1,35 @@
-import { useLoaderData } from "react-router-dom";
+import SectionFinance from "../../../components/sections/finance/finance";
+import API from "../../../services/api/api";
+import { walletResponse } from "../../../services/api/endpoints/wallet";
+import Auth from "../../../services/auth/auth";
+import { redirect, useLoaderData } from "react-router-dom";
 
-export type DataLoaderPageFinance = {};
+export type DataLoaderPageFinance = {
+  walletValues: walletResponse;
+};
 
 export async function loaderPageFinance(): Promise<
   DataLoaderPageFinance | Response
 > {
-  return {};
+  const authRes = Auth.getAuth();
+
+  if (!authRes || Auth.isTokenExpired(authRes.token)) return redirect("/login");
+
+  const walletValues = await API.private.getWallet(authRes);
+
+  return {
+    walletValues: walletValues,
+  };
 }
 
 export default function PageFinance() {
-  const loaderData = useLoaderData() as DataLoaderPageFinance;
+  const dataLoader = useLoaderData() as DataLoaderPageFinance;
 
-  return <main></main>;
+  const { walletValues } = dataLoader;
+
+  return (
+    <main>
+      <SectionFinance walletValues={walletValues} />
+    </main>
+  );
 }
